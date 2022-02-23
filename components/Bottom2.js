dcbc316d85cd9d1499434faf5b288ee8471d67a5
24chris <40150356+24chris@users.noreply.github.com>
2022-02-23 06:53:35 +0300

    Update Bottom2.js

--- components/Bottom2.js
+++ components/Bottom2.js
@@ -116,7 +116,7 @@ const Bottom2 = () => {
                         reprehenderit in voluptate velit esse cillum dolore eu
                         fugiat nulla pariatur. Excepteur sint occaecat cupidatat
                         non proident, sunt in culpa qui officia deserunt mollit
-                        anim id est laborum.
+                        anim id est laborum. nmklp
                       </p>
                     </div>
 
